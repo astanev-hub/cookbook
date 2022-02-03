d022cc8043a70c99a674978c7a64b00eac0e8e82
Alexandre Stanev <astanev@hotmail.com>
2022-02-03 11:39:59 -0300

    Update CookBook.java

--- src/com/principal/CookBook.java
+++ src/com/principal/CookBook.java
@@ -3,6 +3,8 @@ package com.principal;
 public class CookBook {
 
 	public static void main(String[] args) {
+
+//		RECEITA 1		
 		Rendimento rendimento1 = new Rendimento(4, Tipo.COPOS);
 		
 		Ingredientes[] ingredientes1 = new Ingredientes[2];
@@ -20,7 +22,31 @@ public class CookBook {
 		receita1.setPreparo(preparo1);
 		receita1.setIngrediente(ingredientes1);
 
+//		RECEITA 2	
+		Rendimento rendimento2 = new Rendimento(4, Tipo.PORÇÕES);
+		
+		Ingredientes[] ingredientes2 = new Ingredientes[2];
+		ingredientes2[0] = new Ingredientes("Milho de Pipoca", 4, Tipo.COLHERES);
+		ingredientes2[1] = new Ingredientes("Sal", 2, Tipo.COLHERES);
+		
+		Preparo[] preparo2 = new Preparo[5];
+		preparo2[0] = new Preparo("1 - Colocar o milho na panela");
+		preparo2[1] = new Preparo("2 - Ligar o fogo");
+		preparo2[2] = new Preparo("3 - Mexer a panela");
+		preparo2[3] = new Preparo("4 - Deixar ligado o fogo até parar de estourar os milhos");
+		preparo2[4] = new Preparo("5 - Colocar sal e servir");
+
+		Receita receita2 = new Receita(2, "Pipoca", Categoria.SALGADO, 5, rendimento2);
+		receita2.setPreparo(preparo2);
+		receita2.setIngrediente(ingredientes2);
+
+		
+		System.out.println("========= NOVA RECEITA ============");
 		System.out.println(receita1.toString());
+		System.out.println("======== FIM DA RECEITA ===========\n");
+		System.out.println("========= NOVA RECEITA ============");
+		System.out.println(receita2.toString());
+		System.out.println("======== FIM DA RECEITA ===========\n");
 		
 	}
 
